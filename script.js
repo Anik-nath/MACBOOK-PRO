80b7a5d5586a33f5ea610512714b6edb501797f6
ANIK <anikdevnath03112000@gmail.com>
2021-08-21 14:37:41 +0600

    more js is added

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ const SSD1TB = document.getElementById('ssd-1tb');
 const deliveryTwenty = document.getElementById('delivery-twenty');
 const deliveryZero = document.getElementById('delivery-zero');
 const deliveryCharge = document.getElementById('delivery-charge');
+const anotherTotal = document.getElementById('another-total-cost');
+const promoCode = document.getElementById('promo-code');
+const applyButton = document.getElementById('apply-button');
+
 
 //convert all cost value 
 function convert(kartext){
@@ -30,6 +34,8 @@ function click(whose,cost){
     const totalCost = bestPriceValue + memoryCostValue + storageCostValue + deliveryChargeValue;
     //set calculation value
     totalcostField.innerText = totalCost;
+    //another
+    anotherTotal.innerText = totalCost;
 }
 
 //event function for each button
@@ -45,3 +51,16 @@ another(SSD512,storageCost,200);
 another(SSD1TB,storageCost,180);
 another(deliveryZero,deliveryCharge,0);
 another(deliveryTwenty,deliveryCharge,20);
+//promo code
+applyButton.addEventListener('click',function(){
+    
+    promoCodeValue = promoCode.value;
+    if(promoCodeValue == 'stevekaku'){
+        console.log('matched');
+        const previousTotal = convert(anotherTotal);
+        console.log(previousTotal - (previousTotal * 0.20))
+    }
+    else{
+        console.log('not')
+    }
+})
